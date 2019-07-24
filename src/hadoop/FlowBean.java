3147ea9dce3dc63251287353f811057475e4f385
Agony <qing3gan@gmail.com>
2019-07-24 14:53:21 +0800

    FlowCount for key partition.

---
+++ src/hadoop/FlowBean.java
@@ -0,0 +1,93 @@
+package hadoop;
+
+import org.apache.hadoop.io.WritableComparable;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+/**
+ * Created by Agony on 2019/7/23
+ */
+public class FlowBean implements WritableComparable<FlowBean> {
+
+    private String phone;
+    private long upFlow;
+    private long downFlow;
+    private long sumFlow;
+
+    public FlowBean() {
+    }
+
+    public FlowBean(long upFlow, long downFlow) {
+        this.upFlow = upFlow;
+        this.downFlow = downFlow;
+        this.sumFlow = upFlow + downFlow;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public long getUpFlow() {
+        return upFlow;
+    }
+
+    public void setUpFlow(long upFlow) {
+        this.upFlow = upFlow;
+    }
+
+    public long getDownFlow() {
+        return downFlow;
+    }
+
+    public void setDownFlow(long downFlow) {
+        this.downFlow = downFlow;
+    }
+
+    public long getSumFlow() {
+        return sumFlow;
+    }
+
+    public void setSumFlow(long sumFlow) {
+        this.sumFlow = sumFlow;
+    }
+
+    @Override
+    public int compareTo(FlowBean o) {
+        return this.sumFlow > o.sumFlow ? -1 : 1;
+    }
+
+    @Override
+    public void write(DataOutput dataOutput) throws IOException {
+        dataOutput.writeLong(upFlow);
+        dataOutput.writeLong(downFlow);
+        dataOutput.writeLong(sumFlow);
+    }
+
+    @Override
+    public void readFields(DataInput dataInput) throws IOException {
+        upFlow = dataInput.readLong();
+        downFlow = dataInput.readLong();
+        sumFlow = dataInput.readLong();
+    }
+
+    @Override
+    public String toString() {
+        return upFlow + "\t" + downFlow + "\t" + sumFlow;
+    }
+
+    public static FlowBean parse(String value) {
+        String[] line = value.split("\t");
+        String phone = line[1];
+        long upFlow = Long.valueOf(line[7]);
+        long downFlow = Long.valueOf(line[8]);
+        FlowBean flowBean = new FlowBean(upFlow, downFlow);
+        flowBean.setPhone(phone);
+        return flowBean;
+    }
+}
---
+++ src/hadoop/FlowSortJob.java
@@ -0,0 +1,51 @@
+package hadoop;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+import java.io.IOException;
+
+/**
+ * Created by Agony on 2019/7/19
+ */
+public class FlowSortJob {
+
+    static class FlowSortMapper extends Mapper<LongWritable, Text, FlowBean, Text> {
+        @Override
+        protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+            FlowBean flowBean = FlowBean.parse(value.toString());
+            context.write(flowBean, new Text(flowBean.getPhone()));
+        }
+    }
+
+    static class FlowSortReduce extends Reducer<FlowBean, Text, Text, FlowBean> {
+        @Override
+        protected void reduce(FlowBean key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+            context.write(values.iterator().next(), key);
+        }
+    }
+
+    public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
+        Configuration conf = new Configuration();
+        Job job = Job.getInstance(conf);
+        job.setJar("/root/FlowCount.jar");
+        job.setJarByClass(FlowSortJob.class);
+        job.setMapperClass(FlowSortMapper.class);
+        job.setReducerClass(FlowSortReduce.class);
+        job.setMapOutputKeyClass(FlowBean.class);
+        job.setMapOutputValueClass(Text.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(FlowBean.class);
+        FileInputFormat.setInputPaths(job, "hdfs://hadoop01:9000/flowcount/input/flow.log");
+        FileOutputFormat.setOutputPath(job, new Path("hdfs://hadoop01:9000/flowcount/output"));
+        boolean exit = job.waitForCompletion(true);
+        System.exit(exit ? 0 : 1);
+    }
+}
