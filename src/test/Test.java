60481f6e2df42ff0a93d9049e0cb162f67e2c973
Agony <qing3gan@gmail.com>
2019-08-14 15:26:44 +0800

    Relationship.

--- src/test/Test.java
+++ src/test/Test.java
@@ -1,5 +1,9 @@
 package test;
 
+import com.beust.jcommander.internal.Lists;
+
+import java.util.List;
+
 /**
  * Created by Agony on 2018/5/25.
  */
@@ -46,5 +50,11 @@ public class Test {
         System.out.println(a.next());
         System.out.println(a.next());
         System.out.println(b.next());*/
+        List<String> list = Lists.newArrayList("1", "2", "3");
+        for (int i = 0; i < list.size(); i++) {
+            for (int j = i + 1; j < list.size(); j++) {
+                System.out.println(list.get(i) + "-" + list.get(j));
+            }
+        }
     }
 }
