39709af648d4356658891b87ba2fe37fb73cf381
Agony <qing3gan@gmail.com>
2019-09-03 20:29:01 +0800

    Hive udf.

---
+++ src/hive/Sex.java
@@ -0,0 +1,18 @@
+package hive;
+
+import org.apache.hadoop.hive.ql.exec.UDF;
+import org.apache.hadoop.io.Text;
+
+/**
+ * Created by Agony on 2019/8/28
+ */
+public class Sex extends UDF {
+
+    public Text evaluate(final Text s) {
+        return s == null ? null : s.toString().equals("男") ? new Text("male") : new Text("female");
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
